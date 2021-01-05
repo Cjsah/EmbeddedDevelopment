b1ea744cd6705170dc17f7b5843e9ae706dd24b7
OrangeFlavoredDerek <derekchan0407@gmail.com>
2021-01-05 14:40:52 +0800

    # update

---
+++ user/main.c
@@ -0,0 +1,10 @@
+/**
+  * @file       main.c/h
+  * @brief      stm32初始化以及开始任务freeRTOS。h文件定义相关全局宏定义以及
+  *             typedef 一些常用数据类型
+  * @note       
+  */
+
+ #include "main.h"
+
+#include "stm32f4xx.h"
